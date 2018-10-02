6abaff92e0f6e60bdbcc47a3842d6bdbc856678e
Ryan Ureta <rcureta95@gmail.com>
2018-10-02 09:00:14 -0700

    initial

---
+++ scratch.js
@@ -0,0 +1,128 @@
+function isEven(value){
+  if (value % 2 == 0){
+    return true;
+  }
+  else
+    return false;
+}
+
+// o(1). constant, no looping
+
+function areYouHere(arr1, arr2) {
+    for (let i=0; i<arr1.length; i++) {
+        const el1 = arr1[i];
+        for (let j=0; j<arr2.length; j++) {
+            const el2 = arr2[j];
+            if (el1 === el2) return true;
+        }
+    }
+    return false;
+}
+
+// o(n^2). for-loop in line 14 nested inside the for-loop on line 12
+
+function doubleArrayValues(array) {
+    for (let i=0; i<array.length; i++) {
+        array[i] *= 2;
+    }
+    return array;
+}
+
+//o(n). linear, singular for-loop on line 25
+
+function naiveSearch(array, item) {
+    for (let i=0; i<array.length; i++) {
+        if (array[i] === item) {
+            return i;
+        }
+    }
+}
+
+//o(n). linear, singular for-loop on line 34. if-sstatement on 35 does not add to the complexity
+
+function createPairs(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for(let j = i+1; j < arr.length; j++) {
+            console.log(arr[i] + ", " +  arr[j] );
+        }
+    }
+}
+
+
+//o(n^2). 2-layered nested for-loop, same increment
+
+function generateFib(num) {
+  let result = [];
+  for (let i = 1; i <= num; i++) {
+
+    // we're adding the first item
+    // to the result list, append the
+    // number 0 to results
+    if (i === 1) {
+      result.push(0);
+    }
+    // ...and if it's the second item
+    // append 1
+    else if (i == 2) {
+      result.push(1);
+    }
+
+    // otherwise, sum the two previous result items, and append that value to results.
+    else {
+      result.push(result[i - 2] + result[i - 3]);
+    }
+  }
+  // once the for loop finishes
+  // we return `result`.
+  return result;
+}
+
+//linear, o(n). singular for-loop
+
+function efficientSearch(array, item) {
+    let minIndex = 0;
+    let maxIndex = array.length - 1;
+    let currentIndex;
+    let currentElement;
+
+    while (minIndex <= maxIndex) {
+        currentIndex = Math.floor((minIndex + maxIndex) / 2);
+        currentElement = array[currentIndex];
+
+        if (currentElement < item) {
+            minIndex = currentIndex + 1;
+        }
+        else if (currentElement > item) {
+            maxIndex = currentIndex - 1;
+        }
+        else {
+            return currentIndex;
+        }
+    }
+    return -1;
+}
+
+//o(log(n)). cuts sample in half
+
+function findRandomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+//o(1). constant, no looping or iteration
+
+function isPrime(n) {
+    // if n is less than 2 or a decimal, it's not prime
+    if (n < 2 || n % 1 != 0) {
+        return false;
+    }
+    // otherwise, check if `n` is divisible by any integer
+    // between 2 and n.
+    for (let i = 2; i < n; ++i) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+//o(n). only one loop, if-statements don't contribute.
+
+
